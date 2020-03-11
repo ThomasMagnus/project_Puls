2cf5f8908c499a5e64a0d868965ef26f9ee21aa2
EldarMagomedov <emmagomedov_95@list.ru>
2020-03-11 21:07:45 +0300

    add slider

--- src/script.js
+++ src/script.js
@@ -1,27 +1,19 @@
-let arrowLeft = document.querySelector('.slider__btn-left');
-let arrowRight = document.querySelector('.slider__btn-right');
-let gallery = document.querySelectorAll('.slider__courusel .slider__items img');
-
-let i = 0;
-
-arrowLeft.onclick = function() {
-    gallery[i].style.display = 'none';
-    i--;
-
-    if (i < 0) {
-        i = gallery.length-1;
-    }
-
-    gallery[i].style.display = 'block';
-}
-
-arrowRight.onclick = function() {
-    gallery[i].style.display = 'none';
-    i++;
-
-    if (i >= gallery.length) {
-        i = 0;
-    }
-
-    gallery[i].style.display = 'block';
-}
+$(document).ready(function(){
+    $('.slider__courusel').slick({
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+  });
